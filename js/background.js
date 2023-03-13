278fa0dd52a205a7cc405cad074cf651ddf90bea
chubxu <chubxu@gmail.com>
2023-03-13 22:51:53 +0800

    feat: 实现根据仓库动态展示commit提交记录

--- js/background.js
+++ js/background.js
@@ -11,26 +11,43 @@ const messageToContentScript = (request, response, error) => {
   });
 }
 
+const getCurrentTabUrl = async () => {
+  let queryOptions = {
+    active: true,
+    currentWindow: true
+  };
+  let [tab] = await chrome.tabs.query(queryOptions);
+  return tab.url;
+}
+
 const contentScriptMessageHandler = async (request) => {
   if (request.type === 'list_commits') {
-    let url = `https://api.github.com/repos/${request.owner}/${request.repo}/commits?since=${request.since}&until=${request.until}&per_page=${request.perPage}`
-    try {
-      let response = await fetch(url, {
-        method: 'GET',
-        headers: {
-          'Accept': 'application/vnd.github+json',
-          'X-GitHub-Api-Version': '2022-11-28',
+    let currentTabUrlPromise = getCurrentTabUrl()
+    currentTabUrlPromise.then(async (currentTabUrl) => {
+      if (currentTabUrl && currentTabUrl.startsWith("https://github.com/")) {
+        let ownerAndRepo = currentTabUrl.replace("https://github.com/", "").split("/")
+        let owner = ownerAndRepo[0]
+        let repo = ownerAndRepo[1]
+        let url = `https://api.github.com/repos/${owner}/${repo}/commits?since=${request.since}&until=${request.until}&per_page=${request.perPage}`
+        try {
+          let response = await fetch(url, {
+            method: 'GET',
+            headers: {
+              'Accept': 'application/vnd.github+json',
+              'X-GitHub-Api-Version': '2022-11-28',
+            }
+          })
+          let res = await response.json()
+          messageToContentScript(request, res, null)
+        } catch (err) {
+          messageToContentScript(request, null, 'Request Github API Failed, the reason is: ' + err)
         }
-      })
-      let res = await response.json()
-      messageToContentScript(request, res, null)
-    } catch (err) {
-      messageToContentScript(request, null, 'Request Github API Failed, the reason is: ' + err)
-    }
+      }
+    })
   }
 }
 
 chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
   sendResponse('')
-	contentScriptMessageHandler(request)
+  contentScriptMessageHandler(request)
 })
