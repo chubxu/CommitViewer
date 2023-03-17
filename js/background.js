65703b269bd020c81f894ce16e539fa3a47450f6
chubxu <chubxu@gmail.com>
2023-03-17 23:23:01 +0800

    feat: 1、完善reload按钮功能，实现点击重新加载数据；2、丰富summary能力，提供简单的统计功能

--- js/background.js
+++ js/background.js
@@ -30,14 +30,14 @@ const contentScriptMessageHandler = async (request) => {
         let repo = ownerAndRepo[1]
         let url = `https://api.github.com/repos/${owner}/${repo}/commits?since=${request.since}&until=${request.until}&per_page=${request.perPage}`
         try {
-          // let response = await fetch(url, {
-          //   method: 'GET',
-          //   headers: {
-          //     'Accept': 'application/vnd.github+json',
-          //     'X-GitHub-Api-Version': '2022-11-28',
-          //   }
-          // })
-          // let res = await response.json()
+          let response = await fetch(url, {
+            method: 'GET',
+            headers: {
+              'Accept': 'application/vnd.github+json',
+              'X-GitHub-Api-Version': '2022-11-28',
+            }
+          })
+          let res = await response.json()
           messageToContentScript(request, res, null)
         } catch (err) {
           messageToContentScript(request, null, 'Request Github API Failed, the reason is: ' + err)
